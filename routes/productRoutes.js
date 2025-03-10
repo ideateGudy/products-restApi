647e73d5761064a99db4f7eb7160760a1080fe97
ideateGudy <azonubigoodnews@gmail.com>
2025-03-10 09:24:54 +0100

    Added / endpoint for getAllProducts

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 
 const productController = require("../controllers/productContoller");
 
-router.get("/products", productController.getAllProducts);
-// router.get("/products/?name", productController.searchProduct);
+router.get(["/products", "/"], productController.getAllProducts);
 router.get("/product/:id", productController.getProductById);
 router.post("/product", productController.createProduct);
 router.put("/product/:id", productController.updateProduct);
+router.patch("/product/:id", productController.partialUpdate);
 router.delete("/product/:id", productController.deleteProduct);
 
 module.exports = router;
