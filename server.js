43fbfb871df41d0c51ca0ad7e435ca3075520867
ideateGudy <azonubigoodnews@gmail.com>
2025-03-10 09:26:47 +0100

    Added more endpoints for productRoutes

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const app = express();
 const port = 3000;
 // const products = require("./products");
 
-const productsRoutes = require("./routes/productRoutes");
+const productRoutes = require("./routes/productRoutes");
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+// app.get("/", (req, res) => {
+//   res.send("hello world");
+// });
 
 app.use(express.json());
-app.use("/api", productsRoutes);
+app.use(["/api", "/", "/products"], productRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
